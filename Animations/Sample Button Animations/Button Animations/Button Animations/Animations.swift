d5bc8113b606b951d5df14a87b058d1c6dd9e98f
Ilyada <ilyadaberdil@gmail.com>
2018-12-09 00:34:04 +0300

    Pulse-Shake-Flash Animations Added

---
+++ Animations/Sample Button Animations/Button Animations/Button Animations/Animations.swift
@@ -0,0 +1,9 @@
+//
+//  Animations.swift
+//  Button Animations
+//
+//  Created by Berdil İlyada Karacam on 9.12.2018.
+//  Copyright © 2018 Berdil İlyada Karacam. All rights reserved.
+//
+
+import Foundation
---
+++ Animations/Sample Button Animations/Button Animations/Button Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Button Animations
+//
+//  Created by Berdil İlyada Karacam on 9.12.2018.
+//  Copyright © 2018 Berdil İlyada Karacam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
